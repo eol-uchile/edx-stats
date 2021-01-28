4d77e882379f44d2708c9209b35a784dc4143e8a
dario617 <spam.dario@gmail.com>
2021-01-28 09:55:25 -0300

    Add default date to download script

--- back/down_an_process.sh
+++ back/down_an_process.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-if [[ ! -z $1 ]]; then
-  echo $1
+
+run(){
   python manage.py downloadlogs --prefix logrotate --from-date "$1" --check-exists
   python manage.py loadlogs
   python manage.py processtimes "$1"
   python manage.py processvisits "$1"
+}
+
+if [[ ! -z $1 ]]; then
+  run $1
 else
-  echo "Fatal Error: date argument unset!" 1>&2;
-  echo "Expected date in format YYYY-MM-DD as first argument";
+  echo "Expected date in format YYYY-MM-DD as first argument" 1>&2;
+  echo "Using default of 3 days ago...";
+  sleep 3 
+  run $(date -d "3 day ago" +%Y-%m-%d)
 fi
