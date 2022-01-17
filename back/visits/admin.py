a0c183d2a1aca9bfd797218e83fc44e9c795a231
IsaiasVenegas <isaias.venegas@ug.uchile.cl>
2022-01-17 13:02:50 +0100

    Created model

--- back/visits/admin.py
+++ back/visits/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from visits.models import VisitOnPage
+from visits.models import VisitOnPage, CompletionOnBlock
 
 
 class VisitsAdmin(admin.ModelAdmin):
     pass
 
+class CompletionsAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(VisitOnPage, VisitsAdmin)
+admin.site.register(CompletionOnBlock, CompletionsAdmin)
